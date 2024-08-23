b537ec2648354f558d1f8ce0f1e83d03469887ae
dheeraj <dhirajjoshi.987@gmail.com>
2024-08-23 08:23:54 +0530

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,22 @@
+let score = "11ab"
+
+console.log(typeof score);
+console.log(typeof (score))
+
+let valueinnumber = number (score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+// "33"=33
+// "33ab"=NaN
+// true = 1 , false = 0
+
+let isLoggedin = 1
+ 
+let bolleanisloggedin = Boolean(isLoggedin)
+console.log(BooleanisLoggedin);
+// gives a value true
+
+
+
+
