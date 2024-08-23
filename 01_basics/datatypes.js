4906432e08b6c65d3ebea62adc53d26fb4ce9b91
dheeraj <dhirajjoshi.987@gmail.com>
2024-08-23 07:01:43 +0530

    learning datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+// "use strict" treat js code in a newer version
+
+// alert (3+3) we are using node js not browser
+
+// console.log(3
+//     +3
+// );
+// code should be simple and easily readable
+
+console.log(dheeraj);
+
+let name = "dheeraj"
+let age = 18
+let isLoggedin = false 
+
+
+// some types of datatypes
+// Number 
+
+console.log(typeof null);
+// object 
+console.log(typeof undefined);
+// undefined 
+
+
