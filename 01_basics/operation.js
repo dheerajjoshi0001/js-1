736bb6eaee1ae9f8fecc9677a73578950bc4b564
dheeraj <dhirajjoshi.987@gmail.com>
2024-08-25 20:49:47 +0530

    starting of operations

---
+++ 01_basics/operation.js
@@ -0,0 +1,27 @@
+let value = 12; 
+let negvalue = -value; // Correct way to negate the value
+console.log(value); // Output: 12 (original value is unchanged)
+
+// Concatenating strings
+
+let str1 = "hello"
+let str2 = " dheeraj"
+
+let str3 = str1 + str2; // Concatenation of two strings
+
+console.log(str3); // Output: "hello dheeraj"
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(8/2);
+// console.log(5%10);
+
+
+
+
+
+
+
+
